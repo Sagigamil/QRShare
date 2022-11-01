65afba3956c929302082df7d13d8b3778a156278
ofir_sharabi <ofir.sharabi93@gmail.com>
2022-11-01 14:27:01 +0200

    ftp client class for sending a file to ftp server

--- src/FTPClient.py
+++ src/FTPClient.py
@@ -1,8 +1,17 @@
+from xmlrpc.client import ProtocolError
+
+from click import password_option
 from ClientInterface import ClientInterface
+from ftplib import FTP_PORT
+import os
 
 class FTPClient(ClientInterface):
-    def __init__(self, ip, port, user='', password=''):
-        # Build ftp object etc
+    def __init__(        # Build ftp object etcself, ip, port, user='', password=''):
+        self.ip = ip
+        self.port = port
+        self.user = user
+        self.password = password
+
         pass
     
     @staticmethod
@@ -11,5 +20,14 @@ class FTPClient(ClientInterface):
         pass
     
     def upload(self, local_file_path):
-        # implement ftp upload logic here. Return remote file path
-        return "\\remote\\" + local_file_path
+        
+        ftp = FTP(self.ip)
+        ftp.login(user = self.user, passwd = self.password)
+        ftp.cwd('/shared')
+        filename = os.path.basename(local_file_path)
+        file = open(local_file_path . 'rb')
+        ftp.storbinary('STOR ' + filename , file)
+        file.close()
+        ftp.quit()
+
+        return "ftp://" + self.user + ":" + self.password + "@" + self.ip + "/shared/" + filename
